75835f07e7cc94eedd6ff1b976120c8ef02456fe
programandoconro <programandoconro@gmail.com>
2019-10-10 04:37:04 +0100

    clean

--- アプリ/小さい/辞書.go
+++ アプリ/小さい/辞書.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -7,7 +6,6 @@ import (
     "os"
     "time"
     "math/rand"
-
 )
 
 func LinesInFile(fileName string) []string {
@@ -24,21 +22,28 @@ func LinesInFile(fileName string) []string {
     return result
 }
 
-func main() {
-
-    // Get count of lines.
+func ranDic () { 
+    
     lines := LinesInFile("データー/JmnedictFurigana.txt")
-	
-	var NLines int
-	fmt.Print("Cantidad de Kanji (漢字の量), filas estudiar del diccionario:       ")
-	fmt.Scanln(&NLines)
+
+        var NLines int
+        fmt.Print("Cantidad de Kanji (漢字の量), filas estudiar del diccionario:       ")
+        fmt.Scanln(&NLines)
 
      for i := 1; i <= NLines ; i++ {
                 s1 := rand.NewSource(time.Now().UnixNano())
-		r1 := rand.New(s1)
-		r := r1.Intn(6394)	
-                fmt.Println(lines[r])		
+                r1 := rand.New(s1)
+                r := r1.Intn(6394)      
+                fmt.Println(lines[r])           
                 time.Sleep(60 * time.Second)
 
       }
 }
+
+func main() {
+ ranDic()
+}
+
+
+
+
