637336e97622ebee7b3382fb34bb81c0d8323f7f
programandoconro <programandoconro@gmail.com>
2019-10-10 05:01:49 +0100

    add feature

--- アプリ/小さい/辞書.go
+++ アプリ/小さい/辞書.go
@@ -1,49 +1,45 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "time"
-    "math/rand"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
 )
 
 func LinesInFile(fileName string) []string {
-    f, _ := os.Open(fileName)
-    // Create new Scanner.
-    scanner := bufio.NewScanner(f)
-    result := []string{}
-    // Use Scan.
-    for scanner.Scan() {
-        line := scanner.Text()
-        // Append line to result.
-        result = append(result, line)
-    }
-    return result
+	f, _ := os.Open(fileName)
+	// Create new Scanner.
+	scanner := bufio.NewScanner(f)
+	result := []string{}
+	// Use Scan.
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Append line to result.
+		result = append(result, line)
+	}
+	return result
 }
 
-func ranDic () { 
-    
-    lines := LinesInFile("データー/JmnedictFurigana.txt")
+func ranDic() {
 
-        var NLines int
-        fmt.Print("Cantidad de Kanji (漢字の量), filas estudiar del diccionario:       ")
-        fmt.Scanln(&NLines)
+	lines := LinesInFile("データー/JmnedictFurigana.txt")
 
-     for i := 1; i <= NLines ; i++ {
-                s1 := rand.NewSource(time.Now().UnixNano())
-                r1 := rand.New(s1)
-                r := r1.Intn(6394)      
-                fmt.Println(lines[r])           
-                time.Sleep(60 * time.Second)
+	var NLines int
+	fmt.Print("Cantidad de Kanji (漢字の量), filas estudiar del diccionario:       ")
+	fmt.Scanln(&NLines)
 
-      }
+	for i := 1; i <= NLines; i++ {
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
+		r := r1.Intn(6394)
+		fmt.Println(lines[r])
+		time.Sleep(60 * time.Second)
+
+	}
 }
 
 func main() {
- ranDic()
+	ranDic()
 }
-
-
-
-
