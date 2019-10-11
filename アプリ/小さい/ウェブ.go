eca220f04becee9c4f02332c1a9ced5d504ba7bf
programandoconro <programandoconro@gmail.com>
2019-10-11 22:54:31 +0100

    add kana lib

--- アプリ/小さい/ウェブ.go
+++ アプリ/小さい/ウェブ.go
@@ -6,10 +6,10 @@ import (
 	"net/http"
 	"strings"
 	"time"
+	"github.com/gojp/kana"
 )
 
-
-      var port string =  ":8107"
+var port string = ":8080"
 
 func main() {
 
@@ -17,26 +17,24 @@ func main() {
 	http.ListenAndServe(port, nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {	
+func HelloServer(w http.ResponseWriter, r *http.Request) {
 	y := time.Now()
-     	z := y.Hour()
+	z := y.Hour()
 
-     	if  z < 19 && z > 6 {
-           fmt.Fprintf(w, "こにちわ %s さん", r.URL.Path)
+	if z < 19 && z > 6 {
+		fmt.Fprintf(w, "こにちわ %s さん", r.URL.Path)
 
-    	} else {
-           	 fmt.Fprintf(w, "こんばんわ %s さん", r.URL.Path)            
-    	}
-
+	} else {
+		fmt.Fprintf(w, "こんばんわ %s さん", r.URL.Path)
+	}
 
 	a := "人一丨口日目儿見凵山出十八木未丶来大亅了子心土冂田思二丁彳行寸寺時卜上丿刀分厶禾私中彐尹事可亻何自乂又皮彼亠方生月門間扌手言女本乙气気干年三耂者刂前勹勿豕冖宀家今下白勺的云牛物立小文矢知入乍作聿書学合"
 
 	s := strings.Split(a, "")
 
 	fmt.Println("せーべーのポーとト番号は" + port + "です")
-
-	fmt.Fprintf(w, "勉強する漢字は:                        ")            
-
+	k := kana.RomajiToHiragana("kanji")
+	fmt.Fprintf(w, "勉強する漢字("+ k +  ")は:                        ")
 
 	for i := 1; i <= 1; i++ {
 		s1 := rand.NewSource(time.Now().UnixNano())
@@ -44,14 +42,8 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 		r := r1.Intn(99)
 		fmt.Fprintf(w, s[r])
 		time.Sleep(5 * time.Second)
-		
 
 	}
 
-
-
-
-
-
-
+	
 }
