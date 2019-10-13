22c253abf076eedc4f8367d2d4cb09ac66c1b856
programandoconro <programandoconro@gmail.com>
2019-10-13 20:22:18 +0100

    romanji to hiragana

---
+++ アプリ/小さい/平仮名.go
@@ -0,0 +1,29 @@
+package main
+
+import ( "fmt"
+	"github.com/gojp/kana"
+
+)
+
+
+func main () {
+for {
+hiragana ()
+}
+}
+
+func hiragana () {
+var message string
+   
+     fmt.Println("Ingresa ROMANJI a transformar a HIRAGANA\n")
+     fmt.Scanln(&message)
+     k := kana.RomajiToHiragana(message)
+     fmt.Println(message + " en Hiragana se escribe:\n")
+     fmt.Println(k)
+     fmt.Println( "\n")
+
+
+}
+
+
+
