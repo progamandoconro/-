70d1b59adbf0e5c86084dda9bdbfcde64d30c62b
programandoconro <programandoconro@gmail.com>
2019-10-13 16:08:58 +0100

    update

--- アプリ/小さい/wix.go
+++ アプリ/小さい/wix.go
@@ -10,19 +10,18 @@ import (
 	"time"
 )
 
-var port string = ":8107"
+var port string = ":8109"
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("Gorilla!\n"))
 	y := time.Now()
 	z := y.Hour()
 
 	if z < 19 && z > 6 {
-		fmt.Fprintf(w, "こにちわ\n", r.URL.Path)
+		w.Write([]byte( "こにちわ\n"))
 
 	} else {
-		fmt.Fprintf(w, "こんばんわ\n")
+		w.Write([]byte( "こんばんわ\n"))
 	}
 
 	a := "人一丨口日目儿見凵山出十八木未丶来大亅了子心土冂田思二丁彳行寸寺時卜上丿刀分厶禾私中彐尹事可亻何自乂又皮彼亠方生月門間扌手言女本乙气気干年三耂者刂前勹勿豕冖宀家今下白勺的云牛物立小文矢知入乍作聿書学合"
@@ -31,7 +30,7 @@ func YourHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("せーべーのポーとト番号は" + port + "です")
 
-	fmt.Fprintf(w, "勉強する漢字は:\n ")
+	w.Write([]byte("勉強する漢字は:\n "))
 
 	for i := 1; i <= 1; i++ {
 		s1 := rand.NewSource(time.Now().UnixNano())
