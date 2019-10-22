3e737fb671fe0b8e5c01749a38ff164fc7554144
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-22 01:55:49 +0100

    Update kanji.go

--- アプリ/大き/kanji.go
+++ アプリ/大き/kanji.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
 	"strings"
 	"time"
 )
@@ -158,16 +156,4 @@ func main() {
 	frequent()
 }
 
-func LinesInFile(fileName string) []string {
-	f, _ := os.Open(fileName)
-	// Create new Scanner.
-	scanner := bufio.NewScanner(f)
-	result := []string{}
-	// Use Scan.
-	for scanner.Scan() {
-		line := scanner.Text()
-		// Append line to result.
-		result = append(result, line)
-	}
-	return result
-}
+
